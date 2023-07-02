fd5786f2967d1a8bb0b30f69240aabf8474e2567
masaki-tsutano <82243882+masaki-tsutano@users.noreply.github.com>
2023-07-02 13:44:16 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // スムーススクロール
 const scroll = new SmoothScroll('a[href*="#"]');
+
+//ページトップへ戻る
+var $pageTop = $('.page-top');
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $pageTop.fadeIn();
+  } else {
+    $pageTop.fadeOut();
+  }
+});
+
+$pageTop.on('click', function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 200);
+  return false;
+});
