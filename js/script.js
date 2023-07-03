5e4fb73e52b42a27d7d6536e32225ce3989ef555
masaki-tsutano <82243882+masaki-tsutano@users.noreply.github.com>
2023-07-04 07:39:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // スムーススクロール
 const scroll = new SmoothScroll('a[href*="#"]');
+
+//スクロールに応じてヘッダーにactiveクラスを付与
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $('#header').addClass('header-active');
+  } else {
+    $('#header').removeClass('header-active');
+  }
+});
